6cb2efa63656292a754921f45b49497e1361d654
Emily Hawkins <EmilyHawkins0110@gmail.com>
2024-07-22 13:48:31 -0700

    Content phrasing updates

--- Dashboard/src/script.js
+++ Dashboard/src/script.js
@@ -1,7 +1,8 @@
-
+const clientId = "3159a79b4a00464a93cff5a703de47ee";
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
 
+// calling spotify API and console logging data
 if (!code) {
     redirectToAuthCodeFlow(clientId);
 } else {
@@ -18,6 +19,7 @@ if (!code) {
     populateUI(profile, tracks, artists);
 }
 
+// redirect to auth flow function (got code form spotify documentation)
 export async function redirectToAuthCodeFlow(clientId) {
     const verifier = generateCodeVerifier(128);
     const challenge = await generateCodeChallenge(verifier);
@@ -35,6 +37,7 @@ export async function redirectToAuthCodeFlow(clientId) {
     document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
 }
 
+// function to generate code verifier (got code form spotify documentation)
 function generateCodeVerifier(length) {
     let text = '';
     let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -45,6 +48,7 @@ function generateCodeVerifier(length) {
     return text;
 }
 
+// function to generate code challenge (got code form spotify documentation)
 async function generateCodeChallenge(codeVerifier) {
     const data = new TextEncoder().encode(codeVerifier);
     const digest = await window.crypto.subtle.digest('SHA-256', data);
@@ -54,6 +58,7 @@ async function generateCodeChallenge(codeVerifier) {
         .replace(/=+$/, '');
 }
 
+// function to get access token (got code from spotify documentation)
 export async function getAccessToken(clientId, code) {
     const verifier = localStorage.getItem("verifier");
 
@@ -74,6 +79,7 @@ export async function getAccessToken(clientId, code) {
     return access_token;
 }
 
+// function calling spotify API to fetch users profile
 async function fetchProfile(token) {
     const result = await fetch("https://api.spotify.com/v1/me", {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
@@ -82,6 +88,7 @@ async function fetchProfile(token) {
     return await result.json();
 }
 
+// function calling spotify API to fetch users top tracks
 async function fetchTopTracks(token){
     const result = await fetch(`https://api.spotify.com/v1/me/top/tracks`, {
         headers: { Authorization: `Bearer ${token}`, },
@@ -90,6 +97,7 @@ async function fetchTopTracks(token){
     return await result.json();
 }
 
+// function calling spotify API to fetch users top artists
 async function fetchTopArtists(token){
     const result = await fetch(`https://api.spotify.com/v1/me/top/artists`, {
         headers: { Authorization: `Bearer ${token}`, },
@@ -98,7 +106,19 @@ async function fetchTopArtists(token){
     return await result.json();
 }
 
+async function search(token, search_item){
+    const fetchURL = encodeURI(`q=${search_item}`);
+    const result = await fetch(`https://api.spotify.com/v1/search?${fetchURL}&type=track`, {
+        headers: { Authorization: `Bearer ${token}`, },
+    });
+
+    return await result.json();
+}
+
+// function to add spotify response data to page
 function populateUI(profile, tracks, artists) {
+
+    document.getElementById("search-button").addEventListener("click", handleSubmit);
     
     // displays profile photo, username, email
     if (profile.images[0]) {
@@ -123,14 +143,16 @@ function populateUI(profile, tracks, artists) {
 
 
     // loops through all returned top tracks, creates card, and adds the name of the song to the list
+    let track_pop_graph = [];
+    let track_graph = [];
     let top_tracks_artists = [];
     for( let item of tracks.items){
-        let track_artists = '';
+        track_graph.push(item.name);
+        track_pop_graph.push(item.popularity)
         for(let i of item.artists){
             top_tracks_artists.push(i.name);
-            track_artists += i.name + ", "
         }
-        createCard(item.album.images[0].url, item.name, track_artists,  "tracks");
+        createCard(item.album.images[0].url, item.name, item.artists[0].name,  item.external_urls.spotify,"tracks");
         for(let i of item.artists){
             top_tracks_artists.push(i.name);
         }
@@ -141,16 +163,18 @@ function populateUI(profile, tracks, artists) {
     let genres = [];
     let artist_pop_graph = [];
     for( let item of artists.items){
-        createCard(item.images[0].url, item.name, "artists");
+        createCard(item.images[0].url, item.name, item.genres[0], item.external_urls.spotify, "artists");
         artist_graph.push(item.name);
         artist_pop_graph.push(item.popularity);
         genres = genres.concat(item.genres);
     }
+
+    // creating pie charts on pages
     compilePieChart(genres, 'genre');
     compilePieChart(top_tracks_artists, 'artist');
 
+    // creating bar chart
     const ctx = document.getElementById('bar-chart');
-
     new Chart(ctx, {
       type: 'bar',
       data: {
@@ -160,6 +184,30 @@ function populateUI(profile, tracks, artists) {
           backgroundColor: 'rgba(30, 215, 96, .4)',
           borderColor: 'rgba(30, 215, 96, 1)',
           data: artist_pop_graph.slice(0, 10),
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+            suggestedMax: 100
+          }
+        }
+      }
+    });
+
+    // creating bar chart
+    const ctx1 = document.getElementById('bar-chart2');
+    new Chart(ctx1, {
+      type: 'bar',
+      data: {
+        labels: track_graph.slice(0, 5),
+        datasets: [{
+          label: 'Song Popularity',
+          backgroundColor: 'rgba(112, 224, 0, .4)',
+          borderColor: 'rgba(112, 224, 0, 1)',
+          data: track_pop_graph.slice(0, 5),
           borderWidth: 1
         }]
       },
@@ -186,7 +234,8 @@ function populateUI(profile, tracks, artists) {
 
 }
 
-function createCard(image, title, text, type) { // add artist, link
+// function to create each card displaying tracks and artists
+function createCard(image, title, text, link, type) { // add artist, link
     
     // creating padding around cards
     const outerDiv = document.createElement('div');
@@ -194,7 +243,7 @@ function createCard(image, title, text, type) { // add artist, link
 
     // creating card box
     const card = document.createElement('div');
-    card.className = "w-64 max-w-xs bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 overflow-hidden hover:shadow-2xl transition-shadow duration-300 ease-in-out";
+    card.className = "w-64 max-w-xs max-h-min bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 overflow-hidden hover:shadow-2xl transition-shadow duration-300 ease-in-out";
 
     // adding image to card box
     const img = document.createElement('img');
@@ -204,26 +253,22 @@ function createCard(image, title, text, type) { // add artist, link
 
     // creating content div 
     const div1 = document.createElement('div');
-    div1.className = "p-5"
+    div1.className = "p-3"
 
     // ading title to content div
-    const cardTitle = document.createElement('h5');
+    const cardTitle = document.createElement('a');
     cardTitle.innerHTML = title;
-    cardTitle.className = "mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white";
+    cardTitle.href = link;
+    cardTitle.className = "mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white";
     div1.appendChild(cardTitle);
 
     // adding subtext to content div
     const cardText = document.createElement('h5');
     cardText.innerHTML = text;
-    cardText.className = "mb-3 font-normal text-gray-700 dark:text-gray-400";
+    cardText.className = "mb-2 text-sm text-gray-700 dark:text-gray-400";
     div1.appendChild(cardText);
 
-    // adding artist name to content div
-    // const artistName = document.createElement('p');
-    // artistName.innerHTML = artist;
-    // artistName.className = "mb-3 font-normal text-gray-700 dark:text-gray-400";
-    // div1.appendChild(artistName);
-
+    // adding card to correct list (artist or track)
     card.appendChild(div1);
     outerDiv.appendChild(card);
     let typeName = type+"-list"
@@ -232,7 +277,10 @@ function createCard(image, title, text, type) { // add artist, link
     return card;
 }
 
+// function to take arrays and create pie charts
 function compilePieChart(items, type){
+
+    // count how many times each item occurs in the array
     var counts = [];
     const visited = new Array(items.length).fill(0)
     for(var i=0; i<items.length; i++){
@@ -247,8 +295,14 @@ function compilePieChart(items, type){
             counts.push([items[i], count])
         }
     }
+
+    // sort array by most popular item
     counts.sort((a, b) =>b[1]-a[1]);
+
+    // shrink array to top 5 items
     counts = counts.slice(0, 5);
+    
+    // create separate arrays to input into pie chart
     var top_items = []
     var top_counts = []
     for(var i=0; i<counts.length; i++){
@@ -256,6 +310,7 @@ function compilePieChart(items, type){
         top_counts.push(counts[i][1]);
     }
 
+    // create pie charts based on data
     if(type == 'genre'){
         const ctx2 = document.getElementById('genre-pie-chart');
   
@@ -295,11 +350,11 @@ function compilePieChart(items, type){
                     label: 'Top Artists Based on Top Songs',
                     data: top_counts,
                     backgroundColor: [
-                        'rgb(0, 222, 18)',
-                        '#70e000',
-                        '#ccff33',
-                        '#0d9e0d',
-                        '#0F7436'
+                        '#006769',
+                        '#40A578',
+                        '#4FC674',
+                        '#9DDE8B',
+                        '#E6FF94'
                     ],
                     borderWidth: 1
                 }]
